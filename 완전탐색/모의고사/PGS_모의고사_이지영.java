0f37e2360d86c91ce311eaff20a2b06073205ce8
gorapaduckoo <jioung93@naver.com>
2024-01-28 04:16:43 +0900

    refactor: 찍은 번호 배열을 필드 변수로 변경

--- 완전탐색/모의고사/PGS_모의고사_이지영.java
+++ 완전탐색/모의고사/PGS_모의고사_이지영.java
@@ -1,28 +1,32 @@
 import java.util.*;
 
 public class PGS_모의고사_이지영 {
-    static class Solution {
+
+    class Solution {
         int N = 3;
         Queue<Integer>[] q;
+        Integer[][] guessAnswer = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
         public int[] solution(int[] answers) {
             int[] score = new int[N];
             List<Integer> winner = new ArrayList<>();
             init();
 
-            for (int answer : answers) {
-                for (int k = 0; k < N; k++) {
-                    if (q[k].peek() == answer) {
+            int maxScore = 0;
+            for (int i=0; i<answers.length; i++) {
+
+                for (int k=0; k<N; k++) {
+                    if(q[k].peek()==answers[i]) {
                         score[k]++;
+                        maxScore = Math.max(maxScore, score[k]);
                     }
 
                     q[k].add(q[k].poll());
                 }
-            }
-
-            int maxScore = 0;
-            for (int i=0; i<N; i++) {
-                maxScore = Math.max(maxScore, score[i]);
             }
 
             for (int i=0; i<N; i++) {
@@ -43,15 +47,8 @@ public class PGS_모의고사_이지영 {
             q = new Queue[N];
             for(int i=0; i<N; i++) {
                 q[i] = new ArrayDeque<>();
+                q[i].addAll(Arrays.asList(guessAnswer[i]));
             }
-
-            Integer[] first = {1, 2, 3, 4, 5};
-            Integer[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-            Integer[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-            q[0].addAll(Arrays.asList(first));
-            q[1].addAll(Arrays.asList(second));
-            q[2].addAll(Arrays.asList(third));
         }
     }
 }
