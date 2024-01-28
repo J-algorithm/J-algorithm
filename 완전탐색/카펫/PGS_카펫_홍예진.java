7bf03bb3833c85c5f90c4d1c726b365b2f8327c2
YeJinHong <teepij@naver.com>
2024-01-28 11:53:28 +0900

    refactor : 변수명 리팩토링

--- 완전탐색/카펫/PGS_카펫_홍예진.java
+++ 완전탐색/카펫/PGS_카펫_홍예진.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        // 세로 : n, 가로 : m (n <= m)
+        // 세로(height) : n, 가로(width) : m (n <= m)
         // 테두리 1줄 => brown = 2n+2m - 4  => n+m = (brown+4)/2
         // yellow + brown = n*m
         
-        int k = (brown + 4)/2; // k = n+m;
-        int n = 3;
-        int m = k - n;
-        while(n < k){
-            if(yellow + brown == m*n) {
+        int sum = (brown + 4)/2; // k = n+m;
+        int height = 3;
+        int width = sum - height;
+        while(height < sum){
+            if(yellow + brown == width*height) {
                 break;
             }
             
-            n++;
-            m = k - n;
+            height++;
+            width = sum - height;
         }
         
-        answer[0] = m;
-        answer[1] = n;
+        answer[0] = width;
+        answer[1] = height;
         
         return answer;
     }
