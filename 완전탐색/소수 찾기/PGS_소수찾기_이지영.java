85e22667a0eaeecc8a3881962ea6b0a2ed58dd0d
gorapaduckoo <jioung93@naver.com>
2024-01-28 16:13:21 +0900

    refactor: 소수 집합 Set을 소수 여부를 저장하는 boolean 배열로 변경

--- 완전탐색/소수 찾기/PGS_소수찾기_이지영.java
+++ 완전탐색/소수 찾기/PGS_소수찾기_이지영.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class PGS_소수찾기_이지영 {
 
     class Solution {
-        int N;
+        int N, MAX = 10_000_000;
         int[] number;
-        boolean[] used;
-        Set<Integer> primes, answer;
+        boolean[] used, isPrime;
+        Set<Integer> answer;
 
         public int solution(String numbers) {
 
@@ -20,7 +20,7 @@ public class PGS_소수찾기_이지영 {
             N = numbers.length();
             number = new int[N];
             used = new boolean[N];
-            primes = new HashSet<>();
+            isPrime = new boolean[MAX];
             answer = new HashSet<>();
 
             for (int i=0; i<N; i++) {
@@ -29,24 +29,26 @@ public class PGS_소수찾기_이지영 {
         }
 
         void getPrimeNumbers() {
-            for (int num=2; num<10000000; num++) {
-                boolean isPrime = true;
+            for (int num=2; num<MAX; num++) {
+                boolean flag = true;
 
                 for (int i=2; i<=Math.sqrt(num); i++) {
                     if(num%i == 0) {
-                        isPrime = false;
+                        flag = false;
                         break;
                     }
                 }
 
-                if(isPrime) primes.add(num);
+                if(flag) {
+                    isPrime[num] = true;
+                }
             }
         }
 
         void pick(int cnt, StringBuilder sb) {
             if(cnt == N) {
                 int num = Integer.parseInt(sb.toString());
-                if (primes.contains(num)) {
+                if (isPrime[num]) {
                     answer.add(num);
                 }
 
@@ -63,7 +65,6 @@ public class PGS_소수찾기_이지영 {
                 sb.setLength(sb.length()-1);
                 pick(cnt+1, sb);
             }
-
         }
     }
 }
