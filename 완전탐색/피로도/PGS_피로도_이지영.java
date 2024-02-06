901ddeb4406a81fc856acc40d6635cd06af308f5
gorapaduckoo <jioung93@naver.com>
2024-02-06 20:58:56 +0900

    refactor: visit() 메서드와 중복되는 로직 제거

--- 완전탐색/피로도/PGS_피로도_이지영.java
+++ 완전탐색/피로도/PGS_피로도_이지영.java
@@ -8,13 +8,7 @@ public class PGS_피로도_이지영 {
 
             init(dungeons);
 
-            for (int start=0; start<N; start++) {
-                if(dungeons[start][0] > k) continue;
-
-                visited[start] = true;
-                visit(1, k-dungeons[start][1], dungeons);
-                visited[start] = false;
-            }
+            visit(0, k, dungeons);
 
             return answer;
         }
