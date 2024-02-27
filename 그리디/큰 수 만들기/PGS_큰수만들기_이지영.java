e36d252a96a4a306ea5cf4d0f00ccdb72e6f6548
gorapaduckoo <jioung93@naver.com>
2024-02-27 20:09:10 +0900

    [PGS] 큰 수 만들기

---
+++ 그리디/큰 수 만들기/PGS_큰수만들기_이지영.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+ublic class PGS_큰수만들기_이지영 {
+
+    class Solution {
+        Stack<Integer> stack;
+        public String solution(String number, int k) {
+            stack = new Stack<>();
+
+            for (int i=0; i<number.length(); i++) {
+                int n = number.charAt(i) - '0';
+                if (stack.isEmpty()) {
+                    stack.push(n);
+                    continue;
+                }
+
+                while (!stack.isEmpty() && stack.peek()<n && k>0) {
+                    stack.pop();
+                    k--;
+                }
+                stack.push(n);
+            }
+
+            while (k>0) {
+                stack.pop();
+                k--;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while(!stack.isEmpty()) {
+                sb.append(stack.pop());
+            }
+            return sb.reverse().toString();
+        }
+    }
+
+
+}
