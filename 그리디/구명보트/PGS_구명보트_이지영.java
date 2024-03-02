0a80b15c9e29f9dbc272cb3e6d34669f53d8f7a6
gorapaduckoo <jioung93@naver.com>
2024-03-03 00:19:19 +0900

    refactor: while문 조건 변경 및 isRescued 배열 제거

--- 그리디/구명보트/PGS_구명보트_이지영.java
+++ 그리디/구명보트/PGS_구명보트_이지영.java
@@ -5,25 +5,16 @@ public class PGS_구명보트_이지영 {
     class Solution {
         public int solution(int[] people, int limit) {
             int answer = 0;
-            boolean[] isRescued = new boolean[people.length];
             Arrays.sort(people);
 
             int start = 0, end = people.length - 1;
 
-            while (start < end) {
+            while (start <= end) {
                 if (people[start] + people[end] <= limit) {
-                    isRescued[start++] = true;
-                    isRescued[end--] = true;
-                } else {
-                    isRescued[end--] = true;
+                    start++;
                 }
-
+                end--;
                 answer++;
-            }
-
-            while(!isRescued[start]) {
-                answer++;
-                isRescued[start] = true;
             }
 
             return answer;
